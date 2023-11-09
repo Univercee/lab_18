db3d7b92fced500a8eb1aac90c873c60c4c2df6d
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2023-11-09 17:01:44 +0400

    check auth

--- views/actions/account-edit.php
+++ views/actions/account-edit.php
@@ -1,5 +1,10 @@
 <?php
 
+//если пользователь не авторизован
+if(!isset($_COOKIE['session_token'])){
+    header('Location: login');
+}
+
 //запрос к базе данных
 $user = new UserController();
 $response = $user->edit($_POST['name'], $_POST['sex'], strtotime($_POST['birthday'])??null, $_POST['address'],
